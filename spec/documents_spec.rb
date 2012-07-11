65fbd7c648e4e12c5b8d087f2c0f34c7ee955529
Tommy Morgan <tommy@wellbredgrapefruit.com>
2012-07-11 10:58:11 -0400

    Singletons aren't really useful here, I guess.

--- spec/documents_spec.rb
+++ spec/documents_spec.rb
@@ -3,7 +3,7 @@ require_relative "../spec_helper"
 describe Asari do
   describe "updating the index" do
     before :each do
-      Asari.search_domain = "testdomain"
+      @asari = Asari.new("testdomain")
       stub_const("HTTParty", double())
       HTTParty.stub(:post).and_return(fake_post_success)
     end
@@ -11,19 +11,19 @@ describe Asari do
     it "allows you to add an item to the index." do
       HTTParty.should_receive(:post).with("http://doc-testdomain.us-east-1.cloudsearch.amazonaws.com/2011-02-01/documents/batch", { :body => [{ "type" => "add", "id" => "1", "version" => 1, "lang" => "en", "fields" => { :name => "fritters"}}].to_json, :headers => { "Content-Type" => "application/json"}})
 
-      expect(Asari.add_item("1", {:name => "fritters"})).to eq(nil)
+      expect(@asari.add_item("1", {:name => "fritters"})).to eq(nil)
     end
 
     it "allows you to update an item to the index." do
       HTTParty.should_receive(:post).with("http://doc-testdomain.us-east-1.cloudsearch.amazonaws.com/2011-02-01/documents/batch", { :body => [{ "type" => "add", "id" => "1", "version" => 1, "lang" => "en", "fields" => { :name => "fritters"}}].to_json, :headers => { "Content-Type" => "application/json"}})
 
-      expect(Asari.update_item("1", {:name => "fritters"})).to eq(nil)
+      expect(@asari.update_item("1", {:name => "fritters"})).to eq(nil)
     end
 
     it "allows you to delete an item from the index." do
       HTTParty.should_receive(:post).with("http://doc-testdomain.us-east-1.cloudsearch.amazonaws.com/2011-02-01/documents/batch", { :body => [{ "type" => "delete", "id" => "1", "version" => 2}].to_json, :headers => { "Content-Type" => "application/json"}})
 
-      expect(Asari.remove_item("1")).to eq(nil)
+      expect(@asari.remove_item("1")).to eq(nil)
     end
 
     describe "when there are issues" do
@@ -32,15 +32,15 @@ describe Asari do
       end
 
       it "raises an exception when you try to add an item to the index" do
-        expect { Asari.add_item("1", {})}.to raise_error(Asari::DocumentUpdateException)
+        expect { @asari.add_item("1", {})}.to raise_error(Asari::DocumentUpdateException)
       end
 
       it "raises an exception when you try to update an item in the index" do
-        expect { Asari.update_item("1", {})}.to raise_error(Asari::DocumentUpdateException)
+        expect { @asari.update_item("1", {})}.to raise_error(Asari::DocumentUpdateException)
       end
 
       it "raises an exception when you try to remove an item from the index" do
-        expect { Asari.remove_item("1")}.to raise_error(Asari::DocumentUpdateException)
+        expect { @asari.remove_item("1")}.to raise_error(Asari::DocumentUpdateException)
       end
 
     end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -3,33 +3,33 @@ require_relative "../spec_helper"
 describe Asari do
   describe "searching" do
     before :each do
-      Asari.search_domain = "testdomain"
+      @asari = Asari.new("testdomain")
       stub_const("HTTParty", double())
       HTTParty.stub(:get).and_return(fake_response)
     end
 
     it "allows you to search." do
       HTTParty.should_receive(:get).with("http://search-testdomain.us-east-1.cloudsearch.amazonaws.com/2011-02-01/search?q=testsearch")
-      Asari.search("testsearch")
+      @asari.search("testsearch")
     end
 
     it "escapes dangerous characters in search terms." do
       HTTParty.should_receive(:get).with("http://search-testdomain.us-east-1.cloudsearch.amazonaws.com/2011-02-01/search?q=testsearch%21")
-      Asari.search("testsearch!")
+      @asari.search("testsearch!")
     end
 
     it "returns a list of document IDs for search results." do
-      expect(Asari.search("testsearch")).to eq(["123","456"])
+      expect(@asari.search("testsearch")).to eq(["123","456"])
     end
 
     it "returns an empty list when no search results are found." do
       HTTParty.stub(:get).and_return(fake_empty_response)
-      expect(Asari.search("testsearch")).to eq([])
+      expect(@asari.search("testsearch")).to eq([])
     end
 
     it "raises an exception if the service errors out." do
       HTTParty.stub(:get).and_return(fake_error_response)
-      expect { Asari.search("testsearch)") }.to raise_error Asari::SearchException
+      expect { @asari.search("testsearch)") }.to raise_error Asari::SearchException
     end
   end
 end

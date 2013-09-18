fa92aa67cc5216d0aa3446af55b27b7ffe12c609
Kyle Meyer <kaiuhl@gmail.com>
2013-09-18 11:20:00 -0700

    Fix nil term error
    Signed-off-by: Tommy Morgan <tommy@wellbredgrapefruit.com>

--- lib/asari.rb
+++ lib/asari.rb
@@ -71,7 +71,7 @@ class Asari
     page_size = options[:page_size].nil? ? 10 : options[:page_size].to_i
 
     url = "http://search-#{search_domain}.#{aws_region}.cloudsearch.amazonaws.com/#{api_version}/search"
-    url += "?q=#{CGI.escape(term)}"
+    url += "?q=#{CGI.escape(term.to_s)}"
     url += "&bq=#{CGI.escape(bq)}" if options[:filter]
     url += "&size=#{page_size}"
     url += "&return-fields=#{options[:return_fields].join ','}" if options[:return_fields]

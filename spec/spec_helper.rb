a2b2e75080dcd17f89a50ab25bc2710eb46715c7
Tomasz Godniak <tomasz.godniak@u2i.com>
2015-06-29 10:29:42 +0200

    Disable deprecation warnings for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
-require 'simplecov'
-SimpleCov.start
 require 'asari'
 require 'rspec/its'
+require 'simplecov'
+SimpleCov.start
 
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
